4d8f1f3f3e9e65759af4e7b8d9cca87c6e143f59
bigwhoman <hm.keshvari@gmail.com>
2023-03-11 00:00:52 +0330

    c

--- HW2/CE417_2_Spring23_HW2/Practical Assignment 2/Q1/alpha_beta.py
+++ HW2/CE417_2_Spring23_HW2/Practical Assignment 2/Q1/alpha_beta.py
@@ -1,4 +1,5 @@
 from Board import BoardUtility
+import numpy as np
 def minimax(board, player_piece, depth, is_maximizing_player, alpha, beta):
     """
     This function run alpha beta puring algorithm and return best next move
@@ -57,14 +58,16 @@ def minimax_prob(board, our_piece, enemy_piece, depth, is_maximizing_player, alp
     :return best_value, best_move: You have to return best next move and its value
     """
     if depth == 0 or BoardUtility.is_terminal_state(board):
-        return BoardUtility.score_position(board,player_piece), None
-
+        return BoardUtility.score_position(board,our_piece), None
+    if np.random.binomial(p = prob) : 
+        valid = np.random.choice(BoardUtility.get_valid_locations)
+        BoardUtility.make_move(game_board=board,row = valid[0],col=valid[1],player=our_piece)
     if is_maximizing_player:
         value = float("-inf")
         best_move = None
         for move in BoardUtility.get_valid_locations():
-            child_node = BoardUtility.make_move(game_board=board, row = move[0],col = move[1],player = player_piece)
-            score, _ = minimax(board = board,player_piece = child_node, depth = depth - 1, alpha = alpha, beta = beta, is_maximizing_player = False)
+            BoardUtility.make_move(game_board=board, row = move[0],col = move[1],player = our_piece)
+            score, _ = minimax_prob(board = board,our_piece = our_piece,enemy_piece=enemy_piece, depth = depth - 1, alpha = alpha, beta = beta, is_maximizing_player = False)
             if score > value:
                 value = score
                 best_move = move
@@ -77,8 +80,8 @@ def minimax_prob(board, our_piece, enemy_piece, depth, is_maximizing_player, alp
         value = float("inf")
         best_move = None
         for move in BoardUtility.get_valid_locations():
-            child_node = BoardUtility.make_move(game_board=board, row = move[0],col = move[1],player = player_piece)
-            score, _ = minimax(board = board, player_piece = child_node, depth = depth - 1, alpha = alpha, beta = beta,is_maximizing_player = True)
+            BoardUtility.make_move(game_board=board, row = move[0],col = move[1],player = enemy_piece)
+            score, _ = minimax_prob(board = board, our_piece= our_piece,enemy_piece=enemy_piece, depth = depth - 1, alpha = alpha, beta = beta,is_maximizing_player = True)
             if score < value:
                 value = score
                 best_move = move
